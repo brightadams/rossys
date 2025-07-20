3cbb4456001dbb49a85bc28e9180ea307a64c8c0
Bright Adams <brightadams58@gmail.com>
2025-07-20 11:24:02 -0300

    &apos; left out corrrected again

--- app/payment/page.js
+++ app/payment/page.js
@@ -132,7 +132,7 @@ export default function MakePayment() {
               {paymentMethod === 'paypal' && (
                 <div className="bg-blue-50 p-6 rounded-lg text-center">
                   <FaPaypal className="text-blue-700 text-4xl mx-auto mb-4" />
-                  <p className="mb-4 text-gray-800">You'll be redirected to PayPal to complete your payment</p>
+                  <p className="mb-4 text-gray-800">You&apos;ll be redirected to PayPal to complete your payment</p>
                   <button className="bg-blue-700 hover:bg-blue-800 text-white px-6 py-2 rounded-lg text-gray-800">
                     Continue to PayPal
                   </button>
